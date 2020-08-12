4ed11ad42996c465a974c6447e49d16570a24ec1
Niceik <58001842+Niceik@users.noreply.github.com>
2020-08-12 23:21:14 +0300

    wayup fills

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.about-skills').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.about-skills-value').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+    });
+    
+    $(window).scroll(function() {
+	    $('.about-skills-change').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+    });
+    
+    $(window).scroll(function() {
+	    $('.section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+    });
+})
